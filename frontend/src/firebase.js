ed197b77660e631d09e5d6058d485576896886bd
rudrakshpnchl <121367757+rudrakshpnchl@users.noreply.github.com>
2026-04-03 02:18:36 +0530

    fixed firebase api .env expose

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8mYF3nrI_kEVNww3mKx6FQMGyW8gWabk",
-  authDomain: "smartbuy-7165e.firebaseapp.com",
-  projectId: "smartbuy-7165e",
-  storageBucket: "smartbuy-7165e.firebasestorage.app",
-  messagingSenderId: "287789143744",
-  appId: "1:287789143744:web:986b14bb33ad8f299c3f25",
-  measurementId: "G-HX8JSCPXXW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
