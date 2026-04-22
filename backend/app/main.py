2ea7edb65f6c2810f0071f5109e2f89c2f3d22b0
rudrakshpnchl <121367757+rudrakshpnchl@users.noreply.github.com>
2026-04-22 21:53:30 +0530

    added frontend production url

--- backend/app/main.py
+++ backend/app/main.py
@@ -109,7 +109,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["https://smartbuy-ai-shop.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
